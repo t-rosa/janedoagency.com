1fbbe22b8e1bbebeb618df288268f743dd8e1afc
Toma Rosa <toma.rosa@protonmail.com>
2022-07-04 16:34:01 +0200

    Add scrollbar styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,13 @@
+import Link from 'next/link'
+
 function Footer() {
-  return <footer className='border-t'>footer</footer>
+  return (
+    <footer className='border-t'>
+      <Link href='#'>
+        <a>back to the top</a>
+      </Link>
+    </footer>
+  )
 }
 
 export default Footer
