83f26ed5682fbd8fefe66dbdbaa64865d0721e27
Toma Rosa <toma.rosa@protonmail.com>
2022-07-04 21:09:15 +0200

    Desktop Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,61 @@
 import clsx from 'clsx'
+import Link from 'next/link'
 import { Dispatch, SetStateAction } from 'react'
 import Logo from './Logo'
+import teamText from 'images/vinyl/text/equipe.svg'
+import servicesText from 'images/vinyl/text/services.svg'
+import achievementsText from 'images/vinyl/text/realisations.svg'
+import blogText from 'images/vinyl/text/blog.svg'
+import contactText from 'images/vinyl/text/contact.svg'
 
 interface Props {
   overlayIsOpen: boolean
   setOverlayIsOpen: Dispatch<SetStateAction<boolean>>
 }
 
+const MENU = [
+  {
+    id: 1,
+    label: 'équipe',
+    path: '/team',
+    vinylText: teamText,
+  },
+  {
+    id: 2,
+    label: 'services',
+    path: '/services',
+    vinylText: servicesText,
+  },
+  {
+    id: 3,
+    label: 'réalisations',
+    path: '/achievements',
+    vinylText: achievementsText,
+  },
+  {
+    id: 4,
+    label: 'blog',
+    path: '/blog',
+    vinylText: blogText,
+  },
+  {
+    id: 5,
+    label: 'contact',
+    path: '/contact',
+    vinylText: contactText,
+  },
+]
+
 function Mobile({ overlayIsOpen, setOverlayIsOpen }: Props) {
   return (
-    <div className='ml-3 flex h-full justify-between sm:hidden'>
-      <Logo withText />
+    <div className='ml-3 flex h-full justify-between lg:hidden'>
+      <nav className='flex items-center'>
+        <Link href='/'>
+          <a>
+            <Logo withText />
+          </a>
+        </Link>
+      </nav>
       <button
         onClick={() => setOverlayIsOpen(!overlayIsOpen)}
         className='grid w-36 place-items-center border-l'
@@ -18,13 +63,13 @@ function Mobile({ overlayIsOpen, setOverlayIsOpen }: Props) {
         <div
           className={clsx(
             overlayIsOpen ? 'translate-y-4' : '-translate-y-2',
-            'col-[1/2] row-[1/2] h-1 w-11/12 bg-white duration-500'
+            'col-[1/2] row-[1/2] h-1 w-9/12 bg-white duration-500'
           )}
         />
         <div
           className={clsx(
             overlayIsOpen ? '-translate-y-4' : 'translate-y-2',
-            'col-[1/2] row-[1/2] h-1 w-11/12 bg-white duration-500'
+            'col-[1/2] row-[1/2] h-1 w-9/12 bg-white duration-500'
           )}
         />
       </button>
@@ -32,12 +77,47 @@ function Mobile({ overlayIsOpen, setOverlayIsOpen }: Props) {
   )
 }
 
-function Tablet({ overlayIsOpen, setOverlayIsOpen }: Props) {
-  return <div className='hidden sm:block xl:hidden'>tablet header</div>
-}
-
 function Desktop({ overlayIsOpen, setOverlayIsOpen }: Props) {
-  return <div className='hidden xl:block'>desktop header</div>
+  return (
+    <div className='ml-10 hidden h-full grid-cols-[auto_10rem] gap-10 lg:grid'>
+      <nav className='col-[1/2] flex items-center justify-between'>
+        <Link href='/'>
+          <a>
+            <Logo withText />
+          </a>
+        </Link>
+        <ul className='flex gap-5 '>
+          {MENU.map((item) => (
+            <li key={item.id}>
+              <Link href={item.path}>
+                <a className='relative before:absolute before:bottom-0 before:h-[1px] before:w-0 before:bg-hover before:duration-500 hover:text-hover before:hover:w-full'>
+                  <div className='text-xl font-light'>0{item.id}.</div>
+                  <div className='font-display text-2xl capitalize'>{item.label}</div>
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <button
+        onClick={() => setOverlayIsOpen(!overlayIsOpen)}
+        className='col-[2/3] grid place-items-center border-l'
+      >
+        <div
+          className={clsx(
+            overlayIsOpen ? 'translate-y-4' : '-translate-y-2',
+            'col-[1/2] row-[1/2] h-1 w-9/12 bg-white duration-500'
+          )}
+        />
+        <div
+          className={clsx(
+            overlayIsOpen ? '-translate-y-4' : 'translate-y-2',
+            'col-[1/2] row-[1/2] h-1 w-9/12 bg-white duration-500'
+          )}
+        />
+      </button>
+    </div>
+  )
 }
 
 function Header({ overlayIsOpen, setOverlayIsOpen }: Props) {
@@ -49,7 +129,6 @@ function Header({ overlayIsOpen, setOverlayIsOpen }: Props) {
       )}
     >
       <Mobile overlayIsOpen={overlayIsOpen} setOverlayIsOpen={setOverlayIsOpen} />
-      <Tablet overlayIsOpen={overlayIsOpen} setOverlayIsOpen={setOverlayIsOpen} />
       <Desktop overlayIsOpen={overlayIsOpen} setOverlayIsOpen={setOverlayIsOpen} />
     </header>
   )
