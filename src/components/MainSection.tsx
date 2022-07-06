454937430ea29de4f2dca1fa61908504ff161125
Toma Rosa <toma.rosa@protonmail.com>
2022-07-06 20:18:59 +0200

    Desktop main section

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -1,6 +1,8 @@
-import Image from 'next/future/image'
+import desktopBackground from 'images/home/main-desktop-bg.webp'
 import mobileBackground from 'images/home/main-mobile-bg.webp'
 import tabletBackground from 'images/home/main-tablet-bg.webp'
+import Image from 'next/future/image'
+
 function Mobile() {
   return (
     <div className='sm:hidden'>
@@ -26,9 +28,10 @@ function Mobile() {
     </div>
   )
 }
+
 function Tablet() {
   return (
-    <div className='hidden sm:block lg:hidden'>
+    <div className='hidden sm:block xl:hidden'>
       <div className='flex justify-end'>
         <Image
           src={tabletBackground}
@@ -40,12 +43,43 @@ function Tablet() {
       </div>
       <div className='absolute inset-0 flex items-center px-5'>
         <div className='grid gap-10 pl-10'>
-          <h1 className='max-w-sm font-display text-6xl font-bold'>Partagez votre voix.</h1>
-          <p className='max-w-xs  text-2xl font-light'>
+          <h1 className='max-w-md font-display text-7xl font-bold lg:max-w-xl lg:text-8xl'>
+            Partagez votre voix.
+          </h1>
+          <p className='max-w-md text-3xl font-light lg:max-w-lg lg:text-4xl'>
             Découvrez notre multitude de services et compétences à travers notre site internet ainsi
             que nos différents réseaux.
           </p>
-          <button className='w-fit self-start border py-3 px-6 font-display text-3xl font-bold uppercase'>
+          <button className='w-fit self-start border py-3 px-6 font-display text-3xl font-bold uppercase hover:border-hover hover:text-hover lg:py-4 lg:px-8 lg:text-4xl'>
+            découvrir
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Desktop() {
+  return (
+    <div className='hidden xl:block'>
+      <div className='flex justify-end'>
+        <Image
+          src={desktopBackground}
+          className=' mix-blend-lighten'
+          alt=''
+          width={800}
+          height={940}
+        />
+      </div>
+      <div className='absolute inset-0 flex items-center px-5'>
+        <div className='grid gap-10 pl-20'>
+          <h1 className='max-w-3xl font-display text-9xl font-bold'>Partagez votre voix.</h1>
+          <p className='max-w-xl  text-3xl font-light'>
+            Découvrez notre multitude de services et compétences à travers notre site internet ainsi
+            que nos différents réseaux. Jane Do c&apos;est l’agence qui vous aidera à trouver votre
+            place, qu’importe votre style !
+          </p>
+          <button className='w-fit self-start border py-4 px-8 font-display text-4xl font-bold uppercase hover:border-hover hover:text-hover'>
             découvrir
           </button>
         </div>
@@ -58,6 +92,7 @@ function MainSection() {
     <section className='relative'>
       <Mobile />
       <Tablet />
+      <Desktop />
     </section>
   )
 }
